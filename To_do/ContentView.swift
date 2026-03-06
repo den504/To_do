53d6e540f43dcea15bf61d478f05f6c167c8eaef
Dennis <den@gmail.com>
2026-03-05 21:34:30 -0800

    Initial Commit

---
+++ To_do/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To_do
+//
+//  Created by Dennis Okafor on 05/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To_do/To_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doApp.swift
+//  To_do
+//
+//  Created by Dennis Okafor on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct To_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
